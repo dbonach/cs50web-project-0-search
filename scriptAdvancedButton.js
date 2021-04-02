9009a742b82a919a11533798e9e60cccd253da8d
dbonach <deivitlopes@gmail.com>
2021-04-02 01:11:35 -0300

    Added javascript to customize advanced search button when it's clicked

--- scriptAdvancedButton.js
+++ scriptAdvancedButton.js
@@ -1,7 +1,17 @@
-document.addEventListener("click", function(event) {
+let clicked = false;
+
+document.addEventListener("mousedown", function(event) {
 
     if (event.target.id == "advanced-button") {
-        event.target.setAttribute("class","active");
+        clicked = true;
+    }
+})
+
+document.addEventListener("mouseup", function(event) {
+
+    if (clicked) {
+        document.getElementById("advanced-button").setAttribute("class","active");
+        clicked = false;
     }
     else {
         document.getElementById("advanced-button").setAttribute("class","deactivated");
