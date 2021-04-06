7b52373d66dc97b00041878fd127a97bb81adfb8
dbonach <deivitlopes@gmail.com>
2021-04-06 00:37:10 -0300

    Add script for clear button on images.html

---
+++ clear.js
@@ -0,0 +1,11 @@
+document.getElementById("clear-button").addEventListener("click", function() {
+    document.getElementById("image-input").value = "";
+    document.getElementById("clear-button").classList.add("invisible");
+    document.getElementById("image-input").style.width = "395px";
+    console.log("clicou");
+});
+
+document.getElementById("image-input").addEventListener("input", function() {
+    document.getElementById("image-input").style.width = "340px";
+    document.getElementById("clear-button").classList.remove("invisible");
+});
--- speechReco.js
+++ speechReco.js
@@ -1,16 +1,17 @@
 document.getElementById("audio-input").addEventListener("click", function() {
 
-    document.querySelector("#audio-input").style.borderRadius = "50%";
-    document.querySelector("#audio-input").style.boxShadow = "0 0 4px 2px #E3E3E3";
-    
     let SpeechRecognition = webkitSpeechRecognition;
     let recognition = new SpeechRecognition();
 
     recognition.onstart = function() {
+        document.querySelector("#audio-input").style.borderRadius = "50%";
+        document.querySelector("#audio-input").style.boxShadow = "0 0 4px 2px red";
+
         console.log("Listening");
     };
 
     recognition.onspeechend = function() {
+        console.log("Done...");
         recognition.stop();
     };
 
@@ -19,10 +20,15 @@ document.getElementById("audio-input").addEventListener("click", function() {
         let confidence = event.results[0][0].confidence;
         console.log(transcript);
         console.log(confidence);
+
         document.getElementById("image-input").value = transcript;
+        document.getElementById("image-input").style.width = "340px";
+        document.getElementById("clear-button").classList.remove("invisible");
 
         document.querySelector("#audio-input").style.borderRadius = "none";
         document.querySelector("#audio-input").style.boxShadow = "none";
+
+        document.GoogleSearch.submit();
     };
 
     recognition.start();
