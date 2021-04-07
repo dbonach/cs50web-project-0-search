2547f10adbe8230af8bfac612eeaada9e3a1fc28
dbonach <deivitlopes@gmail.com>
2021-04-07 01:46:09 -0300

    Add script to disable image search button
    To replicate google's own behavior when there's no input
    a script should disable a submit from the search button.

--- clear.js
+++ clear.js
@@ -1,15 +1,20 @@
+document.getElementById("image-search-button").setAttribute("type", "button");
+
 document.getElementById("clear-button").addEventListener("click", function() {
     document.getElementById("image-input").value = "";
     document.getElementById("clear-button").classList.add("invisible");
     document.getElementById("image-input").style.width = "390px";
+    document.getElementById("image-search-button").setAttribute("type", "button");
     console.log("clicou");
 });
 
 document.getElementById("image-input").addEventListener("input", function() {
     document.getElementById("image-input").style.width = "340px";
     document.getElementById("clear-button").classList.remove("invisible");
+    document.getElementById("image-search-button").setAttribute("type", "submit");
 
     if (!document.getElementById("image-input").value) {
         document.getElementById("clear-button").classList.add("invisible");
+        document.getElementById("image-search-button").setAttribute("type", "button");
     };
 });
