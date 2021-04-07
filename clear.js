9acdf7f75c28858fb382ec3dcda367cd8c0bfa70
dbonach <deivitlopes@gmail.com>
2021-04-07 15:44:47 -0300

    Fix images.html input id reference for other files

--- clear.js
+++ clear.js
@@ -1,19 +1,19 @@
 document.getElementById("image-search-button").setAttribute("type", "button");
 
 document.getElementById("clear-button").addEventListener("click", function() {
-    document.getElementById("input").value = "";
+    document.getElementById("input-image").value = "";
     document.getElementById("clear-button").classList.add("invisible");
-    document.getElementById("input").style.width = "390px";
+    document.getElementById("input-image").style.width = "390px";
     document.getElementById("image-search-button").setAttribute("type", "button");
     console.log("clicou");
 });
 
-document.getElementById("input").addEventListener("input", function() {
-    document.getElementById("input").style.width = "340px";
+document.getElementById("input-image").addEventListener("input", function() {
+    document.getElementById("input-image").style.width = "340px";
     document.getElementById("clear-button").classList.remove("invisible");
     document.getElementById("image-search-button").setAttribute("type", "submit");
 
-    if (!document.getElementById("input").value) {
+    if (!document.getElementById("input-image").value) {
         document.getElementById("clear-button").classList.add("invisible");
         document.getElementById("image-search-button").setAttribute("type", "button");
     };
